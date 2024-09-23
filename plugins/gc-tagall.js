86ee31df88bc051f9a0ad58af743340ad465227a
KillzN <scorpionsheep203@gmail.com>
2024-09-22 23:45:23 -0300

    KILL - BOT

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,9 +1,7 @@
-
-
 const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
   const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
   const tradutor = _translate.plugins.gc_tagall
 
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
@@ -12,17 +10,17 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `${tradutor.texto1[0]} ${pesan}`;
-  let teks = `${tradutor.texto1[1]}  ${oi}\n\n${tradutor.texto1[2]}\n`;
+  const oi = `*𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙎:* ${pesan}`;
+  let teks = `*> Tʜᴇ Sᴜɴ Is Bʀɪɢʜᴛ Bᴜᴛ Nᴏᴛ As Bʀɪɢʜᴛ As Mᴇ ☀️*\n\n ${oi}\n\n➥ _*@ineffable.mvrco:*_\n`;
   for (const mem of participants) {
-    teks += `┣➥🕷️･ @${mem.id.split('@')[0]}\n`;
+    teks += `🥷🏼 ⇝ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* Readz Aleizn\n`;
+  teks += `└ *KɪʟʟBᴏᴛ ⇝@ineffable.mvrco*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.command = /^(tagall|invocar|invocacion|todos|invocación|putos)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
